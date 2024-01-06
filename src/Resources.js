e26ead7e53e609867252b8c72fe3bc10bb2eed0f
John Heger <heger7@gmail.com>
2024-01-06 17:06:16 -0600

    rearranged resources information and images citations

--- src/Resources.js
+++ src/Resources.js
@@ -17,11 +17,14 @@ function Resources() {
             <br />
             <main>
                 <H3>Resources</H3>
-                <p>Emperor information and images extracted from the following websites:</p>
+                <p>Emperor information extracted from the following websites: </p>
                 <ul>
                     <li>https://www.history.com/news/timeline-emperors-roman-republic</li>
                     <li>https://www.thecollector.com/roman-emperors/</li>
                     <li>https://www.metmuseum.org/toah/hd/roru/hd_roru.htm</li>
+                </ul>
+                <p>Emperor images extracted from the following sources: </p>
+                <ul>
                     <li>https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Augustus_of_Prima_Porta_%28inv._2290%29.jpg/1024px-Augustus_of_Prima_Porta_%28inv._2290%29.jpg</li>
                     <li>https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Nero_Glyptothek_Munich_321.jpg/1280px-Nero_Glyptothek_Munich_321.jpg</li>
                     <li>https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/CVT_APX_Amphitheater_Traiansstatue.jpg/1024px-CVT_APX_Amphitheater_Traiansstatue.jpg</li>
