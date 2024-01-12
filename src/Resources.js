160b567fa8a2509da9ecf917bc559e134fa80631
John Heger <heger7@gmail.com>
2024-01-11 20:14:29 -0600

    added another a link for resources

--- src/Resources.js
+++ src/Resources.js
@@ -31,7 +31,9 @@ function Resources() {
                 </ul>
                 <p>Emperor images extracted from the following sources: </p>
                 <ul>
-                    <li>https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Augustus_of_Prima_Porta_%28inv._2290%29.jpg/1024px-Augustus_of_Prima_Porta_%28inv._2290%29.jpg</li>
+                    <li>
+                    <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Augustus_of_Prima_Porta_%28inv._2290%29.jpg/1024px-Augustus_of_Prima_Porta_%28inv._2290%29.jpg">https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Augustus_of_Prima_Porta_%28inv._2290%29.jpg/1024px-Augustus_of_Prima_Porta_%28inv._2290%29.jpg</a>
+                    </li>
                     <li>https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Nero_Glyptothek_Munich_321.jpg/1280px-Nero_Glyptothek_Munich_321.jpg</li>
                     <li>https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/CVT_APX_Amphitheater_Traiansstatue.jpg/1024px-CVT_APX_Amphitheater_Traiansstatue.jpg</li>
                     <li>https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/MSR-ra-61-b-1-DM.jpg/1280px-MSR-ra-61-b-1-DM.jpg</li>
