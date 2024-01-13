a824d10646a6d58177092e77d1bab90c275c9f01
John Heger <heger7@gmail.com>
2024-01-13 10:04:24 -0600

    added styling to pages

--- src/About.js
+++ src/About.js
@@ -8,16 +8,25 @@ function About() {
     font-size: 3rem;
     `
 
+    const P = styled.p`
+    color: red;
+    font-size: 2rem;
+    `
+    const H4 = styled.h4`
+    color: red;
+    font-size: 1rem;
+    `
+
     return (
         <div className='About'>
             <header>
                 <NavBar />
             </header>
             <H2>About the App</H2>
-            <h4>Roman Emperor Introduction</h4>
-            <p>
+            <H4>Roman Emperor Introduction</H4>
+            <P>
             The Roman emperor was the ruler and monarchical head of state of the Roman Empire, starting with the granting of the title augustus to Octavian in 27 BC.[2] The term "emperor" is a modern convention, and did not exist as such during the Empire. Often when a given Roman is described as becoming emperor in English, it reflects his taking of the title augustus and later basileus. Another title often used was imperator, originally a military honorific, and caesar, originally a surname. Early emperors also used the title princeps ("first one") alongside other Republican titles, notably consul and pontifex maximus.
-            </p>
+            </P>
         </div>
 
     )
